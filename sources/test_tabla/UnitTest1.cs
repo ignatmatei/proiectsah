b35d21dc92428494f0f54e416043e073663519d2
ignatmatei <73382815+ignatmatei@users.noreply.github.com>
2020-10-24 17:01:26 +0300

    theory test

--- sources/test_tabla/UnitTest1.cs
+++ sources/test_tabla/UnitTest1.cs
@@ -1,22 +1,37 @@
 using System;
 using Xunit;
 using proiect_sah;
+using System.ComponentModel.DataAnnotations;
 
 namespace test_tabla
 {
     public class TestTablaInceput
     {
-        [Fact]
-        public void TestPionInceput()
+        [Theory]
+      
+        [InlineData(2,column.E,PieceType.Pawn, Color.White)]
+        [InlineData(7, column.E, PieceType.Pawn, Color.Black)]
+        public void TestPionInceput(int linie, column col,PieceType type, Color c)
         {
             Board b = new Board();
-            var piesa = b.GetPieceFrom(2, column.E);
-            Assert.Equal(PieceType.Pawn, piesa.pieceType);
-            Assert.Equal(Color.White, piesa.Color);
-            piesa = b.GetPieceFrom(7, column.H);
-            Assert.Equal(PieceType.Pawn, piesa.pieceType);
-            Assert.Equal(Color.Black, piesa.Color);
+            var piesa= b.GetPieceFrom(linie, col);
+            Assert.Equal(type, piesa.pieceType);
+            Assert.Equal(c, piesa.Color);
+
+            //var piesa = b.GetPieceFrom(2, column.E);
+            //Assert.Equal(PieceType.Pawn, piesa.pieceType);
+            //Assert.Equal(Color.White, piesa.Color);
+            //piesa = b.GetPieceFrom(7, column.H);
+            //Assert.Equal(PieceType.Pawn, piesa.pieceType);
+            //Assert.Equal(Color.Black, piesa.Color);
         }
 
-    }
+
+        [Fact]
+        public void TestPion()
+        {
+
+        }
+
+        }
 }
